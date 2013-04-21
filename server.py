0d15485ab47ab079a4398e209e70ddbcb529ddcf
Mohd Irtefa <irtefa1@illinois.edu>
2013-04-21 15:01:01 -0500

    attached bingscraper with server

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ Server for skoop
 import json
 from flask import Flask
 from flask import request
-from pygoogle import pygoogle
+from utils import bingscraper
 from utils import htmlparser
 from flask import Response
 app = Flask(__name__)
@@ -17,9 +17,7 @@ app = Flask(__name__)
 @app.route('/search')
 def search():
     query = request.args.get('query')
-    g = pygoogle(query)
-    g.pages = 1  # number of pages worth or results
-    urls = g.get_urls()
+    urls = bingscraper.get_urls(query, 1)
     results = []
     for index, url in enumerate(urls):
             result = {}
