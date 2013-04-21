0a662f51a86e907e24ca240e26325e7eb79f1a6f
Mohd Irtefa <irtefa1@illinois.edu>
2013-04-21 12:49:40 -0500

    returns json of url, title and rank

--- server.py
+++ server.py
@@ -5,16 +5,30 @@ Server for skoop
 """
 
 
+import json
 from flask import Flask
 from flask import request
+from pygoogle import pygoogle
+
 app = Flask(__name__)
 
 
 @app.route('/search')
 def search():
     query = request.args.get('query')
-    print query  # insert your function here irtefa
-    return 'Hello World!'  # return encoded json with {title: , content: , url: , rank: }
+    g = pygoogle(query)
+    g.pages = 1  # number of pages worth or results
+    urls = g.get_urls()
+    results = []
+    for index, url in enumerate(urls):
+            result = {}
+            result['title'] = url['title']
+            result['url'] = url['url']
+            result['rank'] = index
+            results.append(result)
+
+    return json.dumps(results)  # return encoded json with {title: , content: , url: , rank: }
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
