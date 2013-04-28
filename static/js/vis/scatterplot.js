6b7a33e5493fb5a078064018ea2b00461e467f52
suharshs <suharshs@gmail.com>
2013-04-28 13:48:50 -0500

    created simple scatterplot viz, need to add contraint input and another route to backend

---
+++ static/js/vis/scatterplot.js
@@ -0,0 +1,32 @@
+var ranks = [];
+var scores = [];
+for (var i = 0; i < results.length; i++){
+    ranks.push(results[i].rank);
+    scores.push(results[i].score);
+}
+
+var r = Raphael(document.getElementById("linechart"), 700, 640);
+
+var chart = r.linechart(0, 0, 600, 500, ranks, [scores], {
+    colors: ['#F00', '#0F0', '#FF0'],
+    symbol: 'circle',
+    axis: '0 1 1 0',
+    axisxstep: 1,
+    nostroke: true
+});
+
+var text = r.text(260,510,"");
+
+for (var j = 0; j <chart.symbols[0].length; j++){
+    chart.symbols[0][j].attr("title", results[j]);
+    chart.symbols[0][j].hover(function(e){
+        var title = this.attrs.title.title
+        text.attr({
+            text: "title: " + title
+        })
+    }, function(e){
+        text.attr({
+            text: ""
+        })
+    });
+}
