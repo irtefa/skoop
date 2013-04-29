bc9fbe3bb689f948163938e0684c7696d6ecd89a
Martin <wozniew1@illinois.edu>
2013-04-28 21:43:12 -0500

    Change title and add link to page in scatterplot

--- static/js/search.js
+++ static/js/search.js
@@ -1,4 +1,3 @@
-
 // Create a visualization from results returned by server, 
 // in response to a user query
 var populateVizData = function(results) {
@@ -7,6 +6,9 @@ var populateVizData = function(results) {
   // Get attributes from results list
   var ranks = _.pluck(results, "rank");
   var scores = _.pluck(results, "score");
+
+  // Clear the existing visualization
+  $("#scatterplot").empty();
 
   var r = Raphael(document.getElementById("scatterplot"), 700, 640);
 
@@ -20,38 +22,22 @@ var populateVizData = function(results) {
 
   var text = r.text(260,510,"");
 
-  // Make a "clearer" function for clearing any given text field
-  // on a Raphael chart. Call result to clear the field
-  var chartTextClearer = function (textField) {
-    return function() {
-      textField.attr({
-          text: ""
+  var setupChartSymbol = function(symbol, i) {
+    symbol.attr("title", results[i].title);
+    symbol.attr("href", results[i].url);
+    symbol.hover(function(e){
+      var title = this.attrs.title.title;
+      text.attr({
+          text: "title: " + title
       });
-    };
-  };
-
-  // Accessor to a text field for a Raphael chart
-  // Initialize with the text field, then call with desired text
-  var chartTextSetter = function (textField) {
-    return function(newText) {
-      textField.attr({
-          text: "title: " + newText
+    }, function() {
+      text.attr({
+        text: ""
       });
-    };
+    });
   };
 
-  var setupChartSymbol = function(symbol) {
-      symbol.attr("title", results[j]);
-      var setTitle = chartTextSetter(text);
-      symbol.hover(function(e){
-          var title = this.attrs.title.title;
-          setTitle(title);
-      }, chartTextClearer(text));
-  };
-
-  for (var j = 0; j <chart.symbols[0].length; j++){
-      setupChartSymbol(chart.symbols[0][j]);
-  }
+  _.each(chart.symbols[0], setupChartSymbol);
 };
 
 
