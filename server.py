137b5d65ea16972b951cff09254cb4a2aed6835b
Mohd Irtefa <irtefa1@illinois.edu>
2013-05-02 12:50:39 -0500

    fixed indentation issues

--- server.py
+++ server.py
@@ -19,10 +19,10 @@ app = Flask(__name__)
 # These are all the classifiers that the client can choose from
 classifiers = [
     #(index, name to display to client, classifier class)
-    (0, "Phrase Count", phraseclassifier.PhraseClassifier), 
+    (0, "Phrase Count", phraseclassifier.PhraseClassifier),
     (1, "Word Count", wordcountclassifier.WordCountClassifier),
     (2, "Dummy", dummy.DummyClassifier),
-    ]
+     ]
 
 
 @app.route('/')
@@ -34,8 +34,6 @@ def index():
 def search():
 
     print "FORM", request.form, request.form.getlist('classifier')
-
-
 
     # TODO clean this up, this shouldn't be in the server code
     query = request.form['query']
@@ -53,29 +51,28 @@ def search():
             if content != "":
                 scorer = PhraseClassifier(query)
 
-                # Classifiers are now given the entire 'request.form' object, which is just 
-                    # all the form fields from the search page, in a dict. They can then extract 
-                    # whatever user input they need to do their job
+                # Classifiers are now given the entire 'request.form' object, which is just
+                # all the form fields from the search page, in a dict. They can then extract
+                # whatever user input they need to do their job
                 # Eventually, we will get these from special forms on the page
                 # For now, they're just the query itself
                 # This also doesn't support using multiple classifiers yet, but it's a start
                 # TO do that, we'll probably have to give a list of indices
                 # Martin: I'll try to finish this part up early this week
-                request.form['phrase'] = query #TODO temporary hack
-                request.form['keyword'] = (query.split())[0] #TODO temporary hack
+                request.form['phrase'] = query   # TODO temporary hack
+                request.form['keyword'] = (query.split())[0]   # TODO temporary hack
 
                 # Get the classifiers selected by user, and instantiate
                 classifier_indices = map(int, request.form.getlist('classifier'))
-                classifier_index = classifier_indices[0] # only take 1 classifier for now, TODO
-                scorer = classifiers[classifier_index][2](request.form) #instantiate the classifier
+                classifier_index = classifier_indices[0]  # only take 1 classifier for now, TODO
+                scorer = classifiers[classifier_index][2](request.form)  # instantiate the classifier
 
                 score = scorer.score_document(result['title'], content, index)
                 result['score'] = score
                 results.append(result)
-                
 
     # return encoded json with {title: , content: , url: , score:, rank: }
-    return Response(json.dumps(results), mimetype='application/json')  
+    return Response(json.dumps(results), mimetype='application/json')
 
 if __name__ == '__main__':
     app.debug = True
