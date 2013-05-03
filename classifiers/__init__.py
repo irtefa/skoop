f664de7050d3280d65b26c3c33a4361f206d9bb6
Mohd Irtefa <irtefa1@illinois.edu>
2013-05-03 00:08:31 -0500

    added sentiment classifier

--- classifiers/__init__.py
+++ classifiers/__init__.py
@@ -1,3 +1,3 @@
 # TODO: no idea why this was needed, but before this the import statement
 # wasn't detecting "dummy" as a module
-__all__ = ["phraseclassifier", "wordcountclassifier", "dummy"]
+__all__ = ["phraseclassifier", "wordcountclassifier", "dummy", "sentimentclassifier"]
